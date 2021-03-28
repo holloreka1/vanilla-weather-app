d12465bdbbb0ef53c9e2a95a020c4d93c9bf7405
holloreka1 <78386584+holloreka1@users.noreply.github.com>
2021-03-28 12:41:18 +0200

    Update date, pressure, humidity, wind

--- src/app.js
+++ src/app.js
@@ -3,6 +3,34 @@ let cityName = "Sydney";
 let units = "metric";
 let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=${units}`;
 
+function formatDate(timestamp) {
+  //Sunday 13:50
+  let date = new Date(timestamp);
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = "0" + hours;
+  }
+
+  let minutes = date.getMinutes();
+  if (minutes === 0) {
+    minutes = minutes + "0";
+  }
+
+  return `${day} ${hours}:${minutes}`;
+}
+
 function fillData(response) {
   console.log(response.data);
 
@@ -21,6 +49,19 @@ function fillData(response) {
 
   let descriptionElement = document.querySelector("#description");
   descriptionElement.innerHTML = response.data.weather[0].description;
+
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity + "%";
+
+  let windElement = document.querySelector("#wind");
+  //imperial?
+  windElement.innerHTML = Math.round(response.data.wind.speed) + "km/h";
+
+  let pressureElement = document.querySelector("#pressure");
+  pressureElement.innerHTML = response.data.main.pressure + "hPa";
+
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 axios.get(url).then(fillData);
